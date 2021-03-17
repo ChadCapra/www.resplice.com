787ac194698adaaa1ec6e34bff8af523c2106899
Marcus Virginia <marcusvirg345@gmail.com>
2021-03-16 23:37:43 -0500

    fix button and field paths

--- packages/resplice/src/routes/auth/start.svelte
+++ packages/resplice/src/routes/auth/start.svelte
@@ -1,6 +1,6 @@
 <script>
-  import Button from '$lib/shared/Button.svelte'
-  import TextField from '$lib/shared/TextField.svelte'
+  import Button from '$lib/common/Button.svelte'
+  import TextField from '$lib/common/form/TextField.svelte'
   import RespliceWide from '$lib/icons/RespliceWide.svelte'
 
   let attribute = ''
