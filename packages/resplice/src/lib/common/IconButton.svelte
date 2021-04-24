1d985ae1e5feec2e606bc6715ce90c103224871c
Marcus Virginia <marcusvirg345@gmail.com>
2021-04-24 13:42:20 -0500

    Add icons

---
+++ packages/resplice/src/lib/common/IconButton.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let Icon: any
+  export let disabled: boolean
+</script>
+
+<button class="shadow rounded-full" {disabled} on:click>
+  <svelte:component this={Icon} width={32} height={32} />
+</button>
---
+++ packages/resplice/src/lib/user/Header.svelte
@@ -0,0 +1,21 @@
+<script>
+  import BackIcon from '$lib/icons/BackIcon.svelte'
+  import Avatar from '$lib/common/Avatar.svelte'
+  import IconButton from '$lib/common/IconButton.svelte'
+  import SettingsIcon from '$lib/common/icons/IconButton.svelte'
+  import type { User } from '$types'
+
+  export let user: User
+</script>
+
+<nav class="flex items-center justify-between">
+  <div class="flex items-center">
+    <BackIcon />
+    <div class="flex items-center">
+      <Avatar uuid={user.uuid} />
+      <p>{user.name}</p>
+    </div>
+  </div>
+  <IconButton Icon={SettingsIcon} />
+
+</nav>
