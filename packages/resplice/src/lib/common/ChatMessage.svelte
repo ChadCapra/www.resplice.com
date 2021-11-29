021b9e168cc0139b91bcccec270d0ebba60fd74f
Marcus Virginia <marcusvirg345@gmail.com>
2021-11-28 22:46:37 -0600

    fix chat type

--- packages/resplice/src/lib/common/ChatMessage.svelte
+++ packages/resplice/src/lib/common/ChatMessage.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
   import { differenceInDays, format } from 'date-fns'
-  import type { Message } from '$types'
+  import type { Chat } from '$types/chat'
   import Avatar from '$lib/common/Avatar.svelte'
-  export let message: Message
-  const ts = new Date(message.timestamp)
+  export let chat: Chat
+  const sa = chat.sent_at
   $: timestamp =
-    differenceInDays(new Date(), ts) < 7
-      ? format(ts, "E 'at' p")
-      : format(ts, "P',' p")
+    differenceInDays(new Date(), sa) < 7
+      ? format(sa, "E 'at' p")
+      : format(sa, "P',' p")
 </script>
 
 <div class="flex items-start space-x-2">
-  <div class="mt-2"><Avatar uuid={message.entity_uuid} size="sm" /></div>
+  <div class="mt-2"><Avatar uuid={chat.entity_uuid} size="sm" /></div>
 
   <div class="flex flex-col">
     <div class="flex space-x-2">
-      <h4 class="font-semibold text-gray-800">{message.entity_name}</h4>
+      <h4 class="font-semibold text-gray-800">{chat.direction}</h4>
       <time class="text-gray-700">
         {timestamp}
       </time>
     </div>
     <p class="bg-gray-200 px-4 py-2 rounded-b-lg rounded-r-lg">
-      {message.content}
+      {chat.content}
     </p>
   </div>
 </div>
--- packages/resplice/src/lib/contacts/ContactChat.svelte
+++ packages/resplice/src/lib/contacts/ContactChat.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   // import VirtualList from 'svelte-tiny-virtual-list'
   import ChatMessage from '$lib/common/ChatMessage.svelte'
-  import type { Message } from '$types'
+  import type { Chat } from '$types/contact'
 
-  export let messages: Message[]
+  export let chats: Chat[]
 </script>
 
 <div class="flex flex-col w-full h-full p-4 overflow-auto">
-  {#each messages as message}
-    <ChatMessage {message} />
+  {#each chats as chat}
+    <ChatMessage {chat} />
   {/each}
   <!-- <VirtualList itemSize={64} itemCount={messages.length} height={600}>
     <div slot="item" let:index let:style {style}>
