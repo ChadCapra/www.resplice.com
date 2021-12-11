a88a2475e972438af3d7e0be1756ac64b49ff50f
Marcus Virginia <marcusvirg345@gmail.com>
2021-12-11 01:11:31 -0600

    go back functionality

--- packages/resplice/src/routes/app/invite/create/__layout.svelte
+++ packages/resplice/src/routes/app/invite/create/__layout.svelte
@@ -7,7 +7,7 @@
 
 <div class="flex flex-col w-full h-full bg-gray-100">
   <nav class="flex-none flex items-center justify-start p-4">
-    <IconButton Icon={BackIcon} />
+    <IconButton Icon={BackIcon} on:click={() => window.history.back()} />
     <h1 class="ml-4 font-semibold text-xl">Create Invite</h1>
   </nav>
   <main class="bg-white rounded-t-3xl flex-1 flex flex-col p-8 overflow-auto">
