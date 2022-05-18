7d7243972e9a40399014255a7ff29f8605701777
Marcus Virginia <marcusvirg345@gmail.com>
2022-05-17 22:59:15 -0500

    update index for utils

--- libs/re-utils/src/index.ts
+++ libs/re-utils/src/index.ts
@@ -1,4 +1,5 @@
 export * from '$utils/converters/base64'
+export * from '$utils/converters/bytes'
 export * from '$utils/data-structures/arrToMap'
 export * from '$utils/data-structures/arrToRecord'
 export * from '$utils/navigator/getCountry'
@@ -8,7 +9,7 @@ export * from '$utils/navigator/isSupported'
 export * from '$utils/number/clamp'
 export * from '$utils/parsers/phone'
 export * from '$utils/text/capitalize'
-export * from '$utils/validation/validateEmail'
+export * from '$utils/validation/email'
 
 // const version = 'Utilities Common v0.1.0'
 // export default version
