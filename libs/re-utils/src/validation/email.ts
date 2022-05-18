e06b2db1759c945373919355b1cdc8f36f76788b
Marcus Virginia <marcusvirg345@gmail.com>
2022-05-17 22:59:03 -0500

    rename email and use different regex

--- libs/re-utils/src/validation/email.ts
+++ libs/re-utils/src/validation/email.ts
@@ -1,7 +1,8 @@
 export function validateEmail(email: string): boolean {
   /* eslint-disable no-control-regex, no-useless-escape */
   // prettier-ignore
-  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
+  return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)
+  // return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
 }
 
 if (import.meta.vitest) {
