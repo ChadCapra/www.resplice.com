f7cde88e0b913b9bff0ade0bf84445ef7246308a
Marcus Virginia <marcusvirg345@gmail.com>
2022-06-25 22:39:07 -0400

    Add invite list and detail pages

---
+++ apps/resplice/src/routes/app/invite/[id].svelte
@@ -0,0 +1,20 @@
+<script lang="ts" context="module">
+  /** @type {import('@sveltejs/kit').Load} */
+  export async function load({ params }) {
+    return {
+      props: {
+        id: parseInt(params.id, 10)
+      }
+    }
+  }
+</script>
+
+<script lang="ts">
+  import profileStore from '$stores/profile';
+  import Header from '$lib/user/Header.svelte'
+</script>
+
+<div class="flex flex-col w-full h-full bg-gray-100">
+  <Header profile={$profileStore} />
+  <main>Invite Detail</main>
+</div>
---
+++ apps/resplice/src/routes/app/invite/list.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import profileStore from '$stores/profile'
+  import Header from '$lib/user/Header.svelte'
+  import { goto } from '$app/navigation'
+</script>
+
+<div class="flex flex-col w-full h-full bg-gray-100">
+  <Header
+    profile={$profileStore}
+    showUser
+    on:back={() => goto('/app/settings')}
+  />
+  <main
+    class="flex-1 overflow-auto relative w-full max-w-xl m-auto py-4 rounded-t-3xl bg-white"
+  >
+    Invite List
+  </main>
+</div>
