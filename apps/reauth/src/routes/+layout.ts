6c5b14ef2dbec2032ae2de646b0d082273a36404
Marcus Virginia <marcusvirg345@gmail.com>
2023-01-13 01:21:54 -0600

    more cleanup

---
+++ apps/reauth/src/routes/+layout.ts
@@ -0,0 +1,13 @@
+import { locale, waitLocale } from 'svelte-i18n'
+import { browser } from '$app/environment'
+import '$lib/i18n'
+import type { LayoutLoad } from './$types'
+
+export const prerender = true
+
+export const load = (async () => {
+	if (browser) {
+		locale.set(window.navigator.language)
+	}
+	await waitLocale()
+}) satisfies LayoutLoad
--- libs/re-utils/vite.config.ts
+++ libs/re-utils/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       entry: 'src/index.ts',
       name: 're-utils',
       formats: ['es'],
-      fileName: (format) => `re-utils.${format}.js`
+      fileName: 're-utils.js'
     },
     rollupOptions: {
       external: Object.keys(pkg.peerDependencies)
